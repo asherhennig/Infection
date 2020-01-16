1b78b83a79b4e026103dab74c38e2c234fbcacd3
Asher Hennig [Student] <asher.hennig@student.sl.on.ca>
2020-01-16 14:37:23 -0500

    able to now shoot bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePosition;
+    public float fireSpeed = 0.75f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!IsInvoking("fireBullet"))
+            {
+                InvokeRepeating("fireBullet", 0f, fireSpeed);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            CancelInvoke("fireBullet");
+        }
+    }
+
+    void fireBullet()
+    {   // 1   
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+        // 2   
+        bullet.transform.position = firePosition.position;
+        // 3   
+        bullet.GetComponent<Rigidbody>().velocity =      
+            transform.forward * 100;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
+
+
+
+
