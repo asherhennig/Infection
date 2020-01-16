38307bd4a4399e7bc91e9ba82af515aaf69a7e1c
Asher Hennig [Student] <asher.hennig@student.sl.on.ca>
2020-01-16 13:26:58 -0500

    now able to move player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        pos.x += speed * Input.GetAxis("Horizontal") * Time.deltaTime;
+        pos.z += speed * Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.position = pos;
+    }
+}
