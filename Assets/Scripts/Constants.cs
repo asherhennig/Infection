d8bd7dc8e5c88ce21b4bd39a73655226b474a076
Gavin Crews <gavin98@live.ca>
2020-01-20 17:11:40 -0500

    added several scripts and base code
    added constants, gunEquipper, pickUp, and player scripts. they have some base code that should be easy to expand on

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    //Weapon Types
+    public const string Pistol = "Pistol";
+    public const string Shotgun = "Shotgun";
+    public const string Granade = "Granade";
+
+    // Pick-ups
+    public const int healthPickUp = 1;
+    public const int granadePickUp = 2;
+
+    //Enemy Types
+    public const string Spider = "Spider";
+
+    public static readonly int[] AllPickUps = new int[2]
+    {
+        healthPickUp,
+        granadePickUp
+    };
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //initialized current health and max health
+    public int curHealth;
+    public int maxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //added damage getter
+    public void takeDamage(int amount)
+    {
+        int healthDamage = amount;
+        curHealth -= healthDamage;
+        if(curHealth <= 0)
+        {
+            Debug.Log("GameOver");
+        }
+    }
+
+    //added max health up function
+    public void maxUp()
+    {
+        maxHealth += 1;
+        curHealth += 1;
+    }
+
+    // added health pick up and it caps at max health
+    public void pickUpHealth()
+    {
+        //pick up only gives one health
+        curHealth += 1;
+        if(curHealth > maxHealth)
+        {
+            //wont go over max health
+            curHealth = maxHealth;
+        }
+    }
+
+    public void PickUpItem(int pickupItem)
+    {
+        switch (pickupItem)
+        {
+            case Constants.healthPickUp:
+                pickUpHealth();
+                break;
+            case Constants.granadePickUp:
+                break;
+            default:
+                Debug.LogError("Bad pickup type passed" + pickupItem);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
