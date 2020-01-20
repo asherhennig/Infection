36c918b9114595873985f03b6c99dfa5c9721ae1
Gavin Crews <gavin98@live.ca>
2020-01-20 16:02:30 -0500

    Update PlayerController.cs
    player looks at cursor and turns smoothly towards it

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
+    public float rotSpeed = 1.0f;
+    public LayerMask layerMask;
 
     private CharacterController characterController;
+
+    private Vector3 currentLookTarget = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +24,29 @@ public class PlayerController : MonoBehaviour
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 
             0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * speed);
+    }
+
+    void FixedUpdate()
+    {
+        //this makes the palyer look at the cursor when its on screen
+        //creating hit and ray variables
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //fires ray from camera and returns hit
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+            if (hit.point != currentLookTarget)
+            {
+
+            }
+        }
+
+        //sets where we want to turn to
+        Vector3 targetPositon = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+        //gives smooth roation
+        Quaternion rotation = Quaternion.LookRotation(targetPositon - transform.position);
+        //turns to cursor 
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotSpeed);
     }
 }
