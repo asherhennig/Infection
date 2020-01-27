d2ef69948c41365bca98f56f469aa7eded16ba46
Gavin Crews [Student] <gavin.crews@Student.SL.On.Ca>
2020-01-27 16:44:30 -0500

    Added personal test scene. fixed pickup spawns and added enemy spanws
    added personal test scene to avoid later merge issues when merging. I also fixed the gamemanager so that items have a respawn delay after being picked up and now enemies also spawn and begin to move towards the player.

--- Assets/Scripts/AI scripts/FollowFood.cs
+++ Assets/Scripts/AI scripts/FollowFood.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FollowFood : MonoBehaviour
 
 {
     public float speed = 1.0f;
     public float accuracy = 0.09f;      //enemy accuracy to player before enemy stops moving
-    public Transform goal;             //goal is hero/player
-
+    public Transform target;             //goal is hero/player
+    public UnityEvent onDestroy;
 
     // LateUpdate for physics
     void LateUpdate()
     {
-        this.transform.LookAt(goal.position);                               //Enemy faces player
-        Vector3 direction = goal.position - this.transform.position;        //enemy direction: where its going MINUS where it is
+        this.transform.LookAt(target.position);                               //Enemy faces player
+        Vector3 direction = target.position - this.transform.position;        //enemy direction: where its going MINUS where it is
         Debug.DrawRay(this.transform.position, direction, Color.green);     //for the intended path
 
         if (direction.magnitude > accuracy)                                 //If direction length is larger than enemy dis from player
 
             this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);       //..Then move towards the goal in 
                                                                                                         // global space
+    }
+
+    public void Die()
+    {
+        onDestroy.Invoke();
+        onDestroy.RemoveAllListeners();
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,8 @@ public class GameManager : MonoBehaviour
 {
     //gameobjects that will be needed in the script
     public GameObject player;
-    public GameObject[] spawnPoints;
+    public GameObject[] itemSpawnPoints;
+    public GameObject[] enemySpawnPoints;
     public GameObject enemy;
     public GameObject pickUpPrefab;
 
@@ -35,7 +36,7 @@ public class GameManager : MonoBehaviour
     {
         actualPickUpTime = Random.Range(pickUpMaxSpawnTime - 3.0f, pickUpMaxSpawnTime);
         actualPickUpTime = Mathf.Abs(actualPickUpTime);
-        
+
     }
 
     // Update is called once per frame
@@ -48,28 +49,73 @@ public class GameManager : MonoBehaviour
         {
             //generates a random number based on the number of spawnpoints we have and
             //assigns one to be the spawn, finally it spawns a pickup
-            int randnum = Random.Range(0, spawnPoints.Length - 1);
-            GameObject spawnLocation = spawnPoints[randnum];
-            pickUp  = Instantiate(pickUpPrefab) as GameObject;
+            int randnum = Random.Range(0, itemSpawnPoints.Length - 1);
+            GameObject spawnLocation = itemSpawnPoints[randnum];
+            pickUp = Instantiate(pickUpPrefab) as GameObject;
             pickUp.transform.position = spawnLocation.transform.position;
             spawnedPickUp = true;
-            Debug.Log("picked up");
+            actualPickUpTime = Random.Range(pickUpMaxSpawnTime - 3.0f, pickUpMaxSpawnTime);
+            actualPickUpTime = Mathf.Abs(actualPickUpTime);
+            Debug.Log("Spawned");
         }
         //checks if the pick up has been picked up
         if (pickUp == null && spawnedPickUp == true)
         {
+            currentPickUpTime = 0;
             spawnedPickUp = false;
             Debug.Log("deactive");
         }
-        ////enemy spawn timer tick
-        //currentSpawnTime += Time.deltaTime;
-        ////checks if its time to spawn
-        //if(currentSpawnTime > generatedSpawnTime)
-        //{
-        //    //if it is sets spawn timer to 0 and new time to spawn
-        //    currentSpawnTime = 0;
-        //    generatedSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        //enemy spawn timer tick
+        currentSpawnTime += Time.deltaTime;
+        //checks if its time to spawn
+        if (currentSpawnTime > generatedSpawnTime)
+        {
+            //if it is sets spawn timer to 0 and new time to spawn
+            currentSpawnTime = 0;
+            generatedSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+            //
+            if (enemiesPerSpawn > 0 && enemiesOnScreen < totalEnemies)
+            {
+                List<int> previousSpawnLocations = new List<int>();
+                if (enemiesPerSpawn > enemySpawnPoints.Length)
+                {
+                    enemiesPerSpawn = enemySpawnPoints.Length - 1;
+                }
 
-        //}
+                enemiesPerSpawn = (enemiesPerSpawn > totalEnemies) ? enemiesPerSpawn - totalEnemies : enemiesPerSpawn;
+                for (int i = 0; i < enemiesPerSpawn; i++)
+                {
+                    if (enemiesOnScreen < maxEnemiesOnScreen)
+                    {
+                        enemiesOnScreen += 1;
+                        int spawnPoint = -1;
+                        while (spawnPoint == -1)
+                        {
+                            int randNum = Random.Range(0, enemySpawnPoints.Length-1);
+                            if (!previousSpawnLocations.Contains(randNum))
+                            {
+                                previousSpawnLocations.Add(randNum);
+                                spawnPoint = randNum;
+                            }
+                        }
+                        GameObject spawnLocation = enemySpawnPoints[spawnPoint];
+                        GameObject newEnemy = Instantiate(enemy) as GameObject;
+                        newEnemy.transform.position = spawnLocation.transform.position;
+                        FollowFood enemyScript = newEnemy.GetComponent<FollowFood>();
+                        enemyScript.target = player.transform;
+                        Vector3 targetRotation = new Vector3(player.transform.position.x,
+                               newEnemy.transform.position.y, player.transform.position.z);
+                        newEnemy.transform.LookAt(targetRotation);
+                        enemyScript.onDestroy.AddListener(enemyDestroyed);
+                    }
+                }
+            }
+
+        }
+    }
+    public void enemyDestroyed()
+    {
+        enemiesOnScreen -= 1;
+        totalEnemies -= 1;
     }
 }
